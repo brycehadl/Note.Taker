ad734246dbdfd18f4e98d1ae5d57e854ca885f51
Bryce Hadl <brycehadl11@gmail.com>
2023-08-30 21:22:57 -0500

    bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Note Taker Starter Code
+# Express.js Challenge: Note Taker
+
+## Table of Contents
+
+ * [Description](#description)
+
+ * [Live-Screen-Recording-of-Application-Functionality](#live-screen-recording-of-application-functionality)
+
+ * [Screenshots](#screenshots)
+
+ * [Technologies-Used](#technologies-used)
+
+ * [Installation](#installation)
+
+ * [Features](#features)
+
+ * [License](#license)
+## Description
+
+I was given the job of making Note Taker, a program that allows users to take and save notes, for this challenge.
+The software saves and retrieves user-generated note data from a JSON file using an Express.js back end.
+
+The front end of the application has already been developed. My responsibility is to construct the back end, link the two, and then publish the completed application to Heroku.
+
+## Screenshots
+
+## Technologies Used
+
+This project uses the file system module express version 4.18.2. Additionally, it uses a cloud platform called Heroku.
+
+## Installation
+. Clone the repo:
+   git clone git@github.com:brycehadl/Note.Taker.git
+
+2. Open in VS Code. If you do not have VS code you must install it.
+
+3. Proceed to install node.js v16 through the terminal. The command to run if you have homebrew should be something like this: brew install node@16. Nevertheless, this might not always work as expected, so you should check the instructions.
+
+
+4. After installing node.js v16, initialize and create a package.json in the terminal by using the command npm init -y. This is where project files will be kept.
+
+## Features
+
+One of the features of this program is that users may create notes from a remote server.  
+
+## License
+
+This application is covered under the MIT License
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,26 +1,77 @@
-// Dependencies
-const express = require('express');
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
 
-// app use express
+const express = require('express');
 const app = express();
 
-// creating environment variable port
-const PORT = process.env.PORT || 3001;
+const allNotes = require('./db/db.json');
 
-
-// asks express to create a route for every file in the 'public' folder and give it a '/' route
-app.use(express.static('public'));
-// sets up express app to handel data parser, middle wear created req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
 
-// routes to route files
-require('./Develop/routes/apiRoutes')(app);
-require('./Develop/routes/htmlRoutes')(app);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
-// app listener - starts the server
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
 app.listen(PORT, () => {
-  console.log(`Server available at localhost${PORT}`);
+    console.log(`API server now on port ${PORT}!`);
 });
